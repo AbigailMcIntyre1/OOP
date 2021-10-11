3a66aae3a13050380e0e8ae58bf7b31f4b698302
Abigail McIntyre <92331860+AbigailMcIntyre1@users.noreply.github.com>
2021-10-11 14:30:20 -0400

    Add files via upload

---
+++ Calculator Project/Main.java
@@ -0,0 +1,12 @@
+// Abigail McIntyre
+// Due 9/9/2021
+// a simple calculator that outputs the sin, cos, sqrt, ln, and their inverses of a number
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        Calculator myFrame = new Calculator(); 
+        System.out.println("Hello world!");
+    }
+}
